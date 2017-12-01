234e40075334e354c2443deb6e51d4320bf52c2e
Harshad Sharma <harshad@sharma.io>
2017-12-01 05:34:11 +0530

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+from __future__ import (
+    absolute_import,
+    print_function
+)
+
+import io
+import re
+from glob import glob
+from os.path import (
+    basename,
+    dirname,
+    join,
+    splitext
+)
+
+from setuptools import find_packages, setup
+
+
+def read(*names, **kwargs):
+    return io.open(
+        join(dirname(__file__), *names),
+        encoding=kwargs.get('encoding', 'utf8')
+    ).read()
+
+
+setup(
+    name='txtrex',
+    version='0.1.0',
+    license='Apache License 2.0',
+    description='Publish a "blog" using DNS TXT records.',
+    author='Harshad Sharma',
+    author_email='harshad@sharma.io',
+    url='https://github.com/hiway/txtrex',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    include_package_data=True,
+    zip_safe=False,
+    keywords=[
+    ],
+    install_requires=[
+        'arrow==0.12.0',
+        'twisted==17.9.0'
+    ],
+    entry_points={
+        'console_scripts': [
+            'txtrex = txtrex.txtrex:main',
+        ]
+    },
+)
